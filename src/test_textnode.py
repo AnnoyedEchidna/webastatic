20d54d74c8af836b11e6356b411cc2ec3c1e5c1e
AnnoyedEchidna <bryce+github@spycy.quest>
2026-05-10 14:46:12 -0700

    added text_node_to_html_node function to textnode.py and added corresponding unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,7 +4,8 @@ TextNode Class tests for equality and disequality.
 import unittest
 
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
+
 
 
 class TestTextNode(unittest.TestCase):
@@ -43,6 +44,43 @@ class TestTextNode(unittest.TestCase):
                          TextType.IMAGE, "src/img/image2")
         self.assertNotEqual(node5, node6)
 
+    def test_text_node_to_html_node(self):
+        #Test plain text 
+        plain_html_node = text_node_to_html_node(TextNode("This is plain text", "plain", None))
+        self.assertEqual(plain_html_node.tag, None)
+        self.assertEqual(plain_html_node.value, "This is plain text")
+
+        #Test bold text
+        bold_html_node = text_node_to_html_node(TextNode("This is bold text", "bold", None))
+        self.assertEqual(bold_html_node.tag, "b")
+        self.assertEqual(bold_html_node.value, "This is bold text")
+
+        #Test italit text
+        italic_html_node = text_node_to_html_node(TextNode("This is italic text", "italic", None))
+        self.assertEqual(italic_html_node.tag, "i")
+        self.assertEqual(italic_html_node.value, "This is italic text")
+
+        #Test code text
+        code_html_node = text_node_to_html_node(TextNode("This is code text", "code", None))
+        self.assertEqual(code_html_node.tag, "code")
+        self.assertEqual(code_html_node.value, "This is code text")
+
+        #Test link text
+        link_html_node = text_node_to_html_node(TextNode("This is link text", "link", "https://localhost:8888"))
+        self.assertEqual(link_html_node.tag, "a")
+        self.assertEqual(link_html_node.value, "This is link text")
+        self.assertEqual(link_html_node.props_to_html(), "href=\"https://localhost:8888\"")
+        
+        #Test img text
+        img_html_node = text_node_to_html_node(TextNode("This is img alt text", "image", "src/img/favicon.jpg"))
+        self.assertEqual(img_html_node.tag, "img")
+        self.assertEqual(img_html_node.value, None)
+        self.assertEqual(img_html_node.props_to_html(), "src=\"src/img/favicon.jpg\" alt=\"This is img alt text\"")    
+
+    def test_text_node_to_html_node_no_type(self):
+        with self.assertRaises(ValueError):
+            untyped_html_node = text_node_to_html_node(TextNode("This is untyped text", None, None))
 
 if __name__ == "__main__":
     unittest.main()
+    
--- src/textnode.py
+++ src/textnode.py
@@ -3,6 +3,7 @@ Create Enum class for the six different types of text for markup.
 """
 
 from enum import Enum
+from leafnode import LeafNode
 
 
 class TextType(Enum):
@@ -34,3 +35,23 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    """
+    This method takes a TextNode object and returns a LeafNode based on the obj.text_type and text atts.
+    """
+    match text_node.text_type:
+        case TextType.PLAIN:
+            return LeafNode(None, text_node.text, None)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text, None)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text, None)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text, None)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise ValueError("Invalid or missing text type")
